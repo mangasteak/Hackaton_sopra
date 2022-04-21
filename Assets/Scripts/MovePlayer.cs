b19f95f98b3e52d50773227077685630de2958c1
Beafowl <matheo.martin@epitech.eu>
2022-04-21 09:53:29 +0200

    [Upload] if that's not uploading i fuck me

---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed;
+    private Rigidbody2D rb;
+    private Vector3 velocity = Vector3.zero;
+    private Animator animator;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float verticalMovement = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        movePlayer(horizontalMovement, verticalMovement);
+    }
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+            animator.SetBool("IsPressed", true);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            animator.SetBool("IsPressed", true);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+            animator.SetBool("IsPressed", true);
+        else if (Input.GetKey(KeyCode.DownArrow))
+            animator.SetBool("IsPressed", true);
+        else
+        {
+            animator.SetBool("IsPressed", false);
+        }
+    }
+
+    void movePlayer(float _horizontalMovement, float _verticalMovement)
+    {
+        Vector3 targetVelocity = new Vector2(_horizontalMovement, _verticalMovement);
+        Vector3 v3Velocity = rb.velocity;
+        transform.Translate(targetVelocity * Time.deltaTime, Space.World);
+    }
+}
